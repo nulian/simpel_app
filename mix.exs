92be5a2205fc8b04366aac2779b14502ba36b615
Peter Arentsen <peter.arentsen@holder.nl>
2017-12-28 12:49:01 +0100

    Downgrade to phoenix 1.2

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule MyApp.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0"},
+      {:phoenix, "~> 1.2.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"}
